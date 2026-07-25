8c9de8cd329298d9dd13fdefa6d5420505d06e74
b7autiful <lovelymira928@gmail.com>
2026-07-25 18:25:48 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1231,7 +1231,7 @@ function Library:AddShimmer(GuiObject: GuiObject, Info: { [string]: any }?)
 
     local BaseColor = Info.BaseColor or "AccentColor"
     local Duration = Info.Duration or 0.6
-    local Interval = Info.Interval or 6
+    local Interval = Info.Interval or 3
 
     local IsText = GuiObject:IsA("TextLabel") or GuiObject:IsA("TextButton")
     local ColorProp = IsText and "TextColor3" or (GuiObject:IsA("ImageLabel") or GuiObject:IsA("ImageButton")) and "ImageColor3" or "BackgroundColor3"
